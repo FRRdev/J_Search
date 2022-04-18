8079721010e6578d986241216affd8090b22557a
Mikhail <mixail.critsyn@mail.ru>
2022-04-18 10:03:00 +0300

    change test config in main

--- main.py
+++ main.py
@@ -24,22 +24,10 @@ app.add_middleware(
 app.add_middleware(SessionMiddleware, secret_key=settings.SECRET_KEY)
 app.include_router(routers.api_router, prefix=settings.API_V1_STR)
 
-if settings.TESTING:
-    register_tortoise(
-        app,
-        config={
-            "connections": {"default": settings.DATABASE_URI_TEST},
-            "apps": {"models": {"models": settings.APPS_MODELS}},
-            "use_tz": True,
-            "timezone": "Asia/Shanghai",
-            "generate_schemas": True,
-        },
-    )
-else:
-    register_tortoise(
-        app,
-        db_url=settings.DATABASE_URI_LOCAL,
-        modules={"models": settings.APPS_MODELS},
-        # generate_schemas=True,
-        add_exception_handlers=True,
-    )
+register_tortoise(
+    app,
+    db_url=settings.DATABASE_URI_LOCAL,
+    modules={"models": settings.APPS_MODELS},
+    # generate_schemas=True,
+    add_exception_handlers=True,
+)
